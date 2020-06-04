a53700a36d9f740a367547865414b0bf14ac8755
Mveliso Mtana <36462899+Mveliso42@users.noreply.github.com>
2020-06-04 22:49:22 +0200

    m_mtana
    Updating the email from 3rd line to 1st line and displaying the email on the output🤦🏽‍♂️

--- scripts/mmtana.py
+++ scripts/mmtana.py
@@ -1,6 +1,6 @@
+email = 'mtanamveliso@gmail.com'
 name = 'Mveliso Mtana'
 hng_id = 'HNG - 02711'
-email = 'mtanamveliso@gmail.com'
 language = 'python'
 
-print('Hello World, this is' + ' ' + name + ' ' + 'with HNGi7 ID' +' '+ hng_id +' '+ 'using' +' '+ language +' '+ 'for stage 2 task.')
+print('Hello World, this is' + ' ' + name + ' ' + 'with HNGi7 ID' +' '+ hng_id +' '+ 'using' +' '+ language +' '+ 'for stage 2 task email' +' '+ email)
